fecd4333705b6319b500791c8251c176c80ec2f2
Jchu4 <jerome.chuame@gmail.com>
2021-05-09 12:53:15 +0800

    Create react app base and navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div>
+      <nav id="nav-color" className="navbar justify-content-between">
+        <a className="navbar-brand" href="#">
+          🚗
+        </a>
+        <ul className="navbar-nav mr-auto">
+          <button className="btn btn-warning px-3 mx-3" type="button">
+            Find cars
+          </button>
+        </ul>
+        <ul className="align-middle m-2">
+          <a>Manage bookings</a>
+        </ul>
+      </nav>
+    </div>
+  );
+}
